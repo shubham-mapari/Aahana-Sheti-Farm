080cec8ddc8cbdc75d8cf3d71b27b1d3f527e526
shubham-mapari <maparishubham23@gmail.com>
2026-01-16 04:55:58 +0530

    Fix back button and logo replacement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { HashRouter, useNavigate, useLocation } from 'react-router-dom';
+import { App as CapacitorApp } from '@capacitor/app';
 import AppRoutes from './routes/AppRoutes';
+
+function BackButtonHandler() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        const backHandler = CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+            if (location.pathname === '/') {
+                // If on home page, exit the app
+                CapacitorApp.exitApp();
+            } else {
+                // Otherwise go back to previous page in history
+                navigate(-1);
+            }
+        });
+
+        return () => {
+            backHandler.remove();
+        };
+    }, [location, navigate]);
+
+    return <AppRoutes />;
+}
 
 function App() {
     return (
         <HashRouter>
-            <AppRoutes />
+            <BackButtonHandler />
         </HashRouter>
     );
 }
